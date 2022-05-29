87be2691fb3e39a69ef572374709790eb5603441
codenameglassy <codenameglassy@gmail.com>
2022-05-29 13:16:25 +0545

    29

--- LimboTry/Assets/locker.cs
+++ LimboTry/Assets/locker.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class locker : MonoBehaviour
 {
     GameObject E;
-
-
+    bool canPress = false;
+    GameObject lockerLocked;
 
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         E = transform.Find("E").gameObject;
+        lockerLocked = GameObject.Find("GunLocker - locked");
 
+    }
+    void Start()
+    {
+
+        lockerLocked.SetActive(false);
         E.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!canPress)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            lockerLocked.SetActive(true);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -27,6 +42,7 @@ public class locker : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             E.SetActive(true);
+            canPress = true;
         }
     }
 
@@ -35,7 +51,16 @@ public class locker : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             E.SetActive(false);
+            canPress = false;
+            lockerLocked.SetActive(false);
         }
 
+    }
+
+    public void ExitLocker()
+    {
+        E.SetActive(false);
+        canPress = false;
+        lockerLocked.SetActive(false);
     }
 }
